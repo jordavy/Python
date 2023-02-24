640eda14e2e775cd8c403bf4a71efa95269241db
jordavy <jodzapataca@unal.edu.co>
2023-02-24 00:11:06 -0500

    first commit

---
+++ src/Calculadora.py
@@ -0,0 +1,20 @@
+class Calculadora:
+    # Calculada Básica  
+    def __init__(self):
+        pass
+    
+    def sumar(self,a,b):
+        suma = a+b
+        return suma
+    
+    def restar(self,a,b):
+        resta = a-b
+        return resta
+    
+    def multiplicar(self,a,b):
+        result_multi = a*b
+        return result_multi
+
+    def dividir(self,a,b):
+        result_div  = a / b
+        return result_div
---
+++ src/Calculos.py
@@ -0,0 +1,9 @@
+from Calculadora import Calculadora
+
+micalcu = Calculadora()
+a1 = micalcu.sumar(12,2)
+print(a1)
+a2 =  micalcu.multiplicar(4,2)
+print(a2)
+a3 = micalcu.dividir(81,4)
+print(a3)
